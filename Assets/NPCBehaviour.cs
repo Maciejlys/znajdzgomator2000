c4925600e71c3a8ba2273ea109ee0f4c7da7b671
Maciej Łyszczarz <62946872+Maciejlys@users.noreply.github.com>
2021-06-20 19:47:32 +0200

    init

---
+++ Assets/NPCBehaviour.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class NPCBehaviour : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+    public Transform[] wayPoints;
+    private Vector3 _currentDest;
+    private States _currentState;
+    private Trigger _trigger;
+    enum States
+    {
+        Patroling,
+        Chasing,
+        Evade
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _trigger = GetComponentInChildren<Trigger>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        SetDestination();
+        StartCoroutine("Move");
+        _currentState = States.Patroling;
+    }
+
+    private void SetDestination()
+    {
+        if (_currentDest == wayPoints[0].position)
+        {
+            _currentDest = wayPoints[1].position;
+            Debug.Log("Going to 1");
+        }
+        else
+        {
+            _currentDest = wayPoints[0].position;
+            Debug.Log("Going to 0");
+        }
+        _navMeshAgent.SetDestination(_currentDest);
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            if (_currentState == States.Chasing)
+            {
+                _navMeshAgent.SetDestination(_trigger.GetPlayerPos());
+                Debug.Log("Chasing");
+                yield return new WaitForSeconds(1f);
+            }
+            else if (_currentState == States.Patroling)
+            {
+                if (Mathf.Abs(Vector3.Distance(_currentDest, transform.position)) < 1)
+                {
+                    SetDestination();
+                }
+                Debug.Log("Patroling");
+                yield return new WaitForSeconds(1f);
+            }
+            else if (_currentState == States.Evade)
+            {
+                if (Mathf.Abs(Vector3.Distance(wayPoints[0].position, transform.position)) < 1)
+                {
+                    _currentState = States.Patroling;
+                }
+                Debug.Log("Evade");
+                _navMeshAgent.SetDestination(wayPoints[0].position);
+                _currentDest = wayPoints[0].position;
+                yield return new WaitForSeconds(1f);
+            }
+        }
+    }
+
+    public void ChangeToChase()
+    {
+        _currentState = States.Chasing;
+    }
+
+    public void ChangeToPatrol()
+    {
+        _currentState = States.Evade;
+    }
+}
---
+++ Assets/Trigger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    private NPCBehaviour _npcBehaviour;
+    private Vector3 playerPos;
+
+    private void Start()
+    {
+        _npcBehaviour = GetComponentInParent<NPCBehaviour>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _npcBehaviour.ChangeToChase();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _npcBehaviour.ChangeToPatrol();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerPos = other.transform.position;
+        }
+    }
+
+    public Vector3 GetPlayerPos()
+    {
+        return playerPos;
+    }
+}
